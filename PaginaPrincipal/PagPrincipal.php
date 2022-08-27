e6a43820d01eb129459f50862ab9dbd36dfb19ae
Lucas Leguizamón <lucaslegui66@gmail.com>
2022-08-26 21:20:59 -0300

    pagprinc

--- PaginaPrincipal/PagPrincipal.php
+++ PaginaPrincipal/PagPrincipal.php
@@ -48,11 +48,37 @@
             </form>
           </div>
         </div>
-      </nav>
+      </nav> 
 
 
 
+
+      
       <div class="container text-center">
+
+      <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
         <h1 > titulo </h1>
         <div class="row">
           <div class="col-lg-4 col-sd-12 col-margin" >
