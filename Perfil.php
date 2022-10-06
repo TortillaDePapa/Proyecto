b1cf5ad7789e5c2bdecf4526a45cc0db0e652070
Lucas Leguizamón <lucaslegui66@gmail.com>
2022-10-06 13:03:56 -0300

    a

--- Perfil.php
+++ Perfil.php
@@ -14,60 +14,61 @@ session_start();
 <html lang="es">
 
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="CSS/principal.css">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="CSS/principal.css">
 
-  <!-- Icon Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <!-- Icon Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-  <!-- Bootstrap JS -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"></script>
-  <title>TITLE</title>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <title>TITLE</title>
 
 
 </head>
 
 <body>
 
-  <!-- Navbar -->
+    <!-- Navbar -->
 
-  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="PagPrincipal.php">AutoServicio</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active " aria-current="page" href="Local.php">Local</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle me-2" href="#" id="navbarDropdown" role="button"
-              data-bs-toggle="dropdown" aria-expanded="false">
-              Categorias
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Limpieza</a> </li>
-              <!-- <li><hr class="dropdown-divider"></li> -->
-              <li><a class="dropdown-item" href="#">Hogar</a></li>
-              <li><a class="dropdown-item" href="#">Carniceria</a></li>
-            </ul>
-          </li>
-        </ul>
-        <input class="form-control me-3" type="search" placeholder="Buscar" aria-label="Search">
-        <button class="btn btn-buttom btn-custom me-1 boton" type="submit"> </button>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="PagPrincipal.php">AutoServicio</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active " aria-current="page" href="Local.php">Local</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle me-2" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Categorias
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Limpieza</a> </li>
+                            <!-- <li><hr class="dropdown-divider"></li> -->
+                            <li><a class="dropdown-item" href="#">Hogar</a></li>
+                            <li><a class="dropdown-item" href="#">Carniceria</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <input class="form-control me-3" type="search" placeholder="Buscar" aria-label="Search">
+                <button class="btn btn-buttom btn-custom me-1 boton" type="submit"> </button>
 
-        <?php
+                <?php
             echo"<button class='btn btn-buttom btn-custom me-1'  type='submit' onclick='Cerrar()'>  <i class='bi bi-box-arrow-in-right'></i> </button>";          
            
             if(isset($_SESSION['CLIENTE'])){
@@ -98,11 +99,11 @@ session_start();
           }
             echo "<button class='btn btn-buttom btn-custom'  type='submit'> <i class='icon bi-cart3'></i> </button>";
            ?>
-      </div>
-    </div>
-  </nav>
+            </div>
+        </div>
+    </nav>
 
-  <?php
+    <?php
   if(!isset($_SESSION['CLIENTE'])){
     header("Location: PagPrincipal.php");
   }
@@ -111,81 +112,79 @@ session_start();
 
 
 
-  <!-- Formato Perfil -->
+    <!-- Formato Perfil -->
 
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12 offset-md-4">
-        <div class="col-12">
-          <div class="col-5">
-
-             <!--   boton de perfil -->
-
-            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-              <li class="nav-item" role="presentation">
-                <button class="nav-link bg-dark" id="pills-profile-tab" data-bs-toggle="pill"
-                  data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile"
-                  aria-selected="false">Perfil</button>
-
-              </li>
-
-                 <!--   espacio en blanco -->
-
-
-              <li class="nav-item" role="presentation">
-                <button class="nav-link bg-light" id="" data-bs-toggle=""
-                  data-bs-target="" type="button" role="tab" aria-controls=""
-                  aria-selected="false" disabled></button>
-
-              </li>
-
-                 <!--   boton de modificar -->
-
-
-              <li class="nav-item" role="presentation">
-                <button class="nav-link bg-dark" id="pills-contact-tab" data-bs-toggle="pill"
-                  data-bs-target="#pills-contact1" type="button" role="tab" aria-controls="pills-contact"
-                  aria-selected="false">Modificar datos</button>
-              </li>
-
-                 <!--   espacio en blanco -->
-
-              <li class="nav-item" role="presentation">
-                <button class="nav-link bg-light" id="" data-bs-toggle=""
-                  data-bs-target="" type="button" role="tab" aria-controls=""
-                  aria-selected="false" disabled></button>
-
-              </li>
-                 <!--   boton de eliminar -->
-
-                 <li class="nav-item" role="presentation">
-                 <button class="nav-link bg-dark" id="pills-home-tab" 
-                 data-bs-toggle="pill" data-bs-target="#pills-home1" type="button" 
-                 role="tab" aria-controls="pills-home" aria-selected="false"> Eliminar cuenta</button>
-                </li>
-
-
-</ul>
-
-<div class="tab-content" id="pills-tabContent">
-
-
-              <!--   Perfil -->
-
-              <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab"
-                tabindex="0">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 offset-md-4">
                 <div class="col-12">
-                  <div class="col-5">
-                    <img class="img-thumbnail"
-                      src="http://pm1.narvii.com/6880/0b2cc13eba0822a642b4d82d241bc695d59e310er1-400-400v2_uhq.jpg"
-                      alt="">
+                    <div class="col-5">
 
-                  </div>
-                  <div class="col-7">
+                        <!--   boton de perfil -->
 
-                    <div class="form-group row">
+                        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link bg-dark" id="pills-profile-tab" data-bs-toggle="pill"
+                                    data-bs-target="#pills-profile" type="button" role="tab"
+                                    aria-controls="pills-profile" aria-selected="false">Perfil</button>
 
-                      <?php
+                            </li>
+
+                            <!--   espacio en blanco -->
+
+
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link bg-light" id="" data-bs-toggle="" data-bs-target=""
+                                    type="button" role="tab" aria-controls="" aria-selected="false" disabled></button>
+
+                            </li>
+
+                            <!--   boton de modificar -->
+
+
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link bg-dark" id="pills-contact-tab" data-bs-toggle="pill"
+                                    data-bs-target="#pills-contact1" type="button" role="tab"
+                                    aria-controls="pills-contact" aria-selected="false">Modificar datos</button>
+                            </li>
+
+                            <!--   espacio en blanco -->
+
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link bg-light" id="" data-bs-toggle="" data-bs-target=""
+                                    type="button" role="tab" aria-controls="" aria-selected="false" disabled></button>
+
+                            </li>
+                            <!--   boton de eliminar -->
+
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link bg-dark" id="pills-home-tab" data-bs-toggle="pill"
+                                    data-bs-target="#pills-home1" type="button" role="tab" aria-controls="pills-home"
+                                    aria-selected="false"> Eliminar cuenta</button>
+                            </li>
+
+
+                        </ul>
+
+                        <div class="tab-content" id="pills-tabContent">
+
+
+                            <!--   Perfil -->
+
+                            <div class="tab-pane fade" id="pills-profile" role="tabpanel"
+                                aria-labelledby="pills-profile-tab" tabindex="0">
+                                <div class="col-12">
+                                    <div class="col-5">
+                                        <img class="img-thumbnail"
+                                            src="http://pm1.narvii.com/6880/0b2cc13eba0822a642b4d82d241bc695d59e310er1-400-400v2_uhq.jpg"
+                                            alt="">
+
+                                    </div>
+                                    <div class="col-7">
+
+                                        <div class="form-group row">
+
+                                            <?php
            echo"       <label for='usuario' class='col-4'></label>";
            echo"       <div class='col-8'>";
            echo"         <input type='hidden' class='form-control' value='".$_SESSION['CLIENTE'] -> getIDPersona()."' >";
@@ -225,31 +224,32 @@ session_start();
                 
                   ?>
 
-                    </div>
-                    <br>
-                    <div class="form-group text-center">
+                                        </div>
+                                        <br>
+                                        <div class="form-group text-center">
 
-                    </div>
-
-
-                  </div>
+                                        </div>
 
 
+                                    </div>
 
-                </div>
-              </div>
 
-              <!--  Modificar -->
 
-              <div class="tab-pane fade" id="pills-contact1" role="tabpanel" aria-labelledby="pills-contact-tab" tabindex="0">
+                                </div>
+                            </div>
 
-                <div class="col-12">
+                            <!--  Modificar -->
 
-                  <div class="col-7">
-                    <div class="form-group row">
-                      <form action="" method="post">
+                            <div class="tab-pane fade" id="pills-contact1" role="tabpanel"
+                                aria-labelledby="pills-contact-tab" tabindex="0">
 
-                      <?php
+                                <div class="col-12">
+
+                                    <div class="col-7">
+                                        <div class="form-group row">
+                                            <form action="" method="post">
+
+                                                <?php
 
               echo"       <label for='usuario' class='col-4'></label>";
               echo"       <div class='col-8'>";
@@ -307,34 +307,36 @@ session_start();
 
                 ?>
 
-                      <br>
+                                                <br>
 
-                      <div class="form-group text-center">
-                        <input type="submit" value="Actualizar datos de la cuenta" name="ActualizarDatos">
-                        
-                      </div>
-                      </form>
+                                                <div class="form-group text-center">
+                                                    <input type="submit" value="Actualizar datos de la cuenta"
+                                                        name="ActualizarDatos">
 
-
-                    </div>
+                                                </div>
+                                            </form>
 
 
+                                        </div>
 
-                  </div>
-                </div>
 
-              </div>
 
-              <!--  Borrar -->
-              
-            <div class="tab-pane fade " id="pills-home1" role="tabpanel" aria-labelledby="pills-home-tab"   >
+                                    </div>
+                                </div>
 
-              <div class="col-12">
+                            </div>
 
-                <div class="col-7">
-                  
-          <form action="" method="post">
- <?php
+                            <!--  Borrar -->
+
+                            <div class="tab-pane fade " id="pills-home1" role="tabpanel"
+                                aria-labelledby="pills-home-tab">
+
+                                <div class="col-12">
+
+                                    <div class="col-7">
+
+                                        <form action="" method="post">
+                                            <?php
  
  echo"       <label for='usuario' class='col-4'></label>";
  echo"       <div class='col-8'>";
@@ -353,41 +355,38 @@ session_start();
  
  
  ?>
-           
-           <br>
 
-<div class="form-group text-center" id="Alert">
-  <input type="submit" value="Eliminar" name="Eliminar" id="LiveAlert" >
-  </form>
-</div>
+                                            <br>
+
+                                            <div class="form-group text-center" id="Alert">
+                                                <input type="submit" value="Eliminar" name="Eliminar" id="LiveAlert">
+                                        </form>
+                                    </div>
 
 
-</div>
-</div>
-</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
 
-      </div>
+        </div>
     </div>
 
-          <script>
+    <script>
     function Cerrar() {
-      var obAjax = new XMLHttpRequest();
-      obAjax.open('POST', 'Persistencia/Control.php', true);
-      obAjax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-      obAjax.onreadystatechange = function () {
-        window.location.reload();
-      }
-      obAjax.send('Cerrar');
+        var obAjax = new XMLHttpRequest();
+        obAjax.open('POST', 'Persistencia/Control.php', true);
+        obAjax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        obAjax.onreadystatechange = function() {
+            window.location.reload();
+        }
+        obAjax.send('Cerrar');
     }
+    </script>
 
-
-
-  </script>
-
-<?php
+    <?php
 if(isset($_POST['Eliminar'])){
   $p = new Persona();
   $p1 = new PersonaBD();
@@ -467,7 +466,7 @@ if(isset($_POST['Eliminar'])){
         </div> -->
 
 
-         <!-- </div>
+<!-- </div>
            <div class="form-group row">
              <label for="email" class="col-4"> email: </label>
              <div class="col-8">
