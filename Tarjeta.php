e62c91f4f009a7cbb970557a9d44bc1f37a7e2f7
Lucas Leguizamón <lucaslegui66@gmail.com>
2022-11-07 05:45:28 -0300

    ugu

--- Tarjeta.php
+++ Tarjeta.php
@@ -53,33 +53,19 @@ if (session_status() == PHP_SESSION_NONE){
   <!-- Example Code -->
 
   <div class="container ">
-  <div class="row">
-        <div class="col-6 text-center">
-        <h2>
-        <svg xmlns="http://www.w3.org/2000/svg" style="margin: 10px !important" width="30" height="30" fill="currentColor"
-        class="bi bi-bag" viewBox="0 0 16 16">
-        <path
-          d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-      </svg>
-      Finalizar Compra
-</h2>
-  
 
-        </div>
+     <div class="text-center">
+ 
+          <h1>
 
-        <div class="col-4 text-center">
-          <h3>
-
-          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+          <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
 </svg>
 Resumen de la compra
-</h3>
-        </div>
-    <!-- <div class="col-sm">s
-     <h1> - </h1> 
-    </div> -->
-  </div>
+</h1>
+
+</div>
+      
 
 
 
@@ -329,7 +315,9 @@ echo "<p> ".$_SESSION['MostrarCarrito'][$i]['Precio']."</p>";
 
   </div>
 
-  <div class="row">
+  <br>
+
+  <div class="row row-precio">
       <div class="col">
         <h5>Precio total: </h5>
 
@@ -429,6 +417,11 @@ document.getElementById('mostrarprecio-div1').innerHTML = '$'+preciof;
 document.getElementById('mostrarprecio-div').innerHTML = '$'+preciof;
 document.getElementById('mostrarprecio-div2').innerHTML = '$'+preciof;
 </script>
+
+<br>
+
+
+
 
 </body>
 
