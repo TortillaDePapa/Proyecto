a9cfcac64458942db8fe913e6ed0db22375cbde1
TortillaDePapa <benjamin25654@gmail.com>
2022-11-10 23:16:08 -0300

    a

--- Persistencia/ControlCompra.php
+++ Persistencia/ControlCompra.php
@@ -6,6 +6,8 @@ if (session_status() == PHP_SESSION_NONE){
 
 
 if(isset($_POST['FinalizarCompra'])){
+    date_default_timezone_set('America/Montevideo');
+
     $newConn = new Conexion(); 
     $newConn -> Conectar();
     $sql = "SELECT idpersona FROM personas WHERE idpersona = '".$_POST['usuario']."'" ;
@@ -19,7 +21,12 @@ if(isset($_POST['FinalizarCompra'])){
                 $sql1  = "INSERT into selecciona(IDCliente,IDproducto,CantidadProducto) VALUES('".$fila['idpersona']."', '".$_SESSION['MostrarCarrito'][$i]['id']."', '".$_SESSION['MostrarCarrito'][$i]['Cantidad']."')";
                 $resultado = mysqli_query($newConn -> conn, $sql1);
                 if($resultado){
-                  
+                    $sql2 = "SELECT idcliente FROM suministra WHERE idpersona = '".$_POST['usuario']."'" ;
+                    $fechaActual = date('d-m-Y H:i:s');
+                    for($i = 0; $i <count($_SESSION['MostrarCarrito']); $i++){
+                        $sql1  = "INSERT into Compra(IDcompra,IDCliente,IDproducto,Fecha,Total,MetodoDePago,MetodoDeEnvio) VALUES('".$fila['idpersona']."', '".$_SESSION['MostrarCarrito'][$i]['id']."', ".$fechaActual.", '".$_SESSION['MostrarCarrito'][$i]['Precio']*$_SESSION['MostrarCarrito'][$i]['Cantidad']."')";
+                    }
+
                 }
             }
         }
